7e09c8e580c4652c5ea54624f9f20679102b1734
limon-chandra-ray <limonroy.19cse013@gmail.com>
2023-11-30 15:05:52 +0600

    'mysql-data-base-add'

--- core/settings.py
+++ core/settings.py
@@ -69,10 +69,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bsksnjpg_psf',
+        'USER': 'bsksnjpg_psf',
+        'PASSWORD': '62fuoh=Y@)X%',
+        'HOST':'localhost',
+        'PORT':'3306',
+        'OPTIONS':{
+            'charset': 'utf8mb4',
+            'collation': 'utf8mb4_unicode_ci',
+        },
     }
 }
 
