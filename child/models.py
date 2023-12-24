4e5477506bb04cdf27e466207f3af2006398e13a
limon-chandra-ray <limonroy.19cse013@gmail.com>
2023-12-24 13:03:58 +0600

    'all-model-image-path-customize'

--- child/models.py
+++ child/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from user.models import CustomUser
+from sadmin.utlis import date_to_str
 from django.dispatch import receiver
 from django.db.models.signals import pre_delete,post_save
 from django.shortcuts import get_object_or_404
 from datetime import date
+from PIL import Image
+import os
 # Create your models here.
 
 class ChildManager(models.Manager):
@@ -19,7 +22,10 @@ class Child(CustomUser):
 def create_child_profile(sender,instance,created,*args, **kwargs):
     if created and instance.role == "CHILD":
         ChildProfile.objects.create(child_user = instance)
-
+def child_image_path(instance,filename):
+    file_name,extension = os.path.splitext(filename)
+    new_file_path = f"{date_to_str()}.{extension}"
+    return os.path.join('children',new_file_path)
 
 
 class ChildProfile(models.Model):
@@ -27,7 +33,7 @@ class ChildProfile(models.Model):
     child_first_name = models.CharField(max_length=250,null=True,blank=True)
     child_last_name = models.CharField(max_length=250,null=True,blank=True)
     child_phone_number = models.CharField(max_length=11,null=True,blank=True)
-    child_image = models.ImageField(upload_to=f'children/{date.today()}/',null=True,blank=True)
+    child_image = models.ImageField(upload_to=child_image_path,null=True,blank=True)
     child_father_name = models.CharField(max_length=250,null=True,blank=True)
     child_mother_name = models.CharField(max_length=250,null=True,blank=True)
     child_date_of_birth = models.DateField(null=True,blank=True)
@@ -49,7 +55,10 @@ class ChildProfile(models.Model):
             if existing.child_image != self.child_image:
                 existing.child_image.delete(save=False)
         super(ChildProfile,self).save(*args, **kwargs)
-
+        if self.child_image:
+            image = Image.open(self.child_image.path)
+            image.thumbnail((300,300))
+            image.save(self.child_image.path)
     @receiver(pre_delete,sender = 'child.ChildProfile')
     def child_profile_image_delete_signal(instance,sender,*args, **kwargs):
         for field in instance._meta.fields:
